90764acb52667139ad6bd75868fd466f81d925ca
ThomasKat <thomas.kathriner@awk.ch>
2018-04-10 10:36:44 +0200

    Change RAM per container to 7 GB

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -46,6 +46,8 @@ c.DockerSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+# Memory / RAM limit per container
+c.DockerSpawner.mem_limit = '7G'
 # volume_driver is no longer a keyword argument to create_container()
 # c.DockerSpawner.extra_create_kwargs.update({ 'volume_driver': 'local' })
 # Remove containers once they are stopped
