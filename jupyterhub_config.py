d34ed7078404f82333d6cb8d23cb086eaec616f1
ThomasKat <thomas.kathriner@awk.ch>
2018-04-17 10:29:11 +0200

    Add support for AWK Textmining 1.0.2

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -22,7 +22,8 @@ c.JupyterHub.last_activity_interval = 5 * 60
 # Key: text to be displayed to the user. Values: exact Docker image
 c.DockerSpawner.image_whitelist = {
     'Vanilla Datascience-Notebook (8d9388cac562, 02.04.18)': 'jupyter/datascience-notebook:8d9388cac562',
-    'AWK Textmining 1.0.1 (02.04.18)': 'awk/textmining:1.0.1'
+    'AWK Textmining 1.0.1 (02.04.18)': 'awk/textmining:1.0.1',
+    'AWK Textmining 1.0.2 (17.04.18)': 'awk/textmining:1.0.2'
 }
 # JupyterHub requires a single-user instance of the Notebook server, so we
 # default to using the `start-singleuser.sh` script included in the
